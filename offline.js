8efdeb6631b42fca88b29317a5a4ba4aa4e138f7
krikkov <98097401+krikkov@users.noreply.github.com>
2024-03-14 17:29:21 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1710412117,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт2-sheet1.png",
+		"images/Спрайт2-sheet2.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт3-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
